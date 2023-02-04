58bdcac1feca59196bc430400a758ea518c79d77
星星 <2064989403@qq.com>
2023-02-05 00:03:46 +0800

    feat: 优化了首页右上角welcome组件结构，完成了post页面底部组件设计

---
+++ src/components/home/card/welocome/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styles from './index.module.less'
+
+interface Props {
+  slogan?: string
+}
+
+const getTimeState = () => {
+  // 获取当前时间小时
+  const hours = new Date().getHours()
+  // 设置默认文字
+  let text = ''
+  // 判断当前时间段
+  if (hours >= 0 && hours <= 10)
+    text = '早上好！'
+  else if (hours > 10 && hours <= 14)
+    text = '中午好！'
+  else if (hours > 14 && hours <= 18)
+    text = '下午好！'
+  else if (hours > 18 && hours <= 24)
+    text = '晚上好！'
+
+  return text
+}
+
+const Welcome: React.FC<Props> = (props: Props) => {
+  const { slogan } = props
+  return (
+        <div className={styles.myCard}>
+            <div>
+                <div className={styles.left}>
+                    <span>{getTimeState()}</span>
+                    <div>{slogan ?? '点亮在社区的每一天'}</div>
+                </div>
+                <button className={styles.right}>
+                    <span>去签到</span>
+                </button>
+            </div>
+        </div>
+  )
+}
+Welcome.displayName = 'Welcome'
+export default Welcome
