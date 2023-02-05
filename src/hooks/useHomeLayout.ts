f65dd7b0e80c8020e9116125370c0df8be91ccbf
煌仔 <7625319+cmhua@user.noreply.gitee.com>
2023-02-05 21:40:49 +0800

    new pull

--- src/hooks/useHomeLayout.ts
+++ src/hooks/useHomeLayout.ts
@@ -6,7 +6,7 @@ export const useHomeLayout = (bNumber = 1, offset = 0) => {
   const [sideFixed, setSideFixed] = useState(false)
   const [isUp, setIsUp] = useState(true)
 
-  // 后期扩展 主导航栏固定, 侧边栏固定高度变化
+  // 后期扩展 主导航栏固定, 侧边栏固定高度变化--
   const sideOffset = bNumber * 220 + offset
   useEffect(() => {
     if (scrollPosition > 1060 + sideOffset)
